d5cba5674078a1b41435f7d51db16d5fbcf6ddcf
Senya Volkov <formobileprima@gmail.com>
2020-06-05 19:13:07 +0300

    Updates

---
+++ precache-manifest.515276d135b858b43a4a7ce09ed4331f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e591487f098703642f148c9fcae2288c",
+    "url": "/kursach/index.html"
+  },
+  {
+    "revision": "6b3e06934cf6b3d7be06",
+    "url": "/kursach/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "19f08c38fbc1e14ab905",
+    "url": "/kursach/static/css/main.063cb193.chunk.css"
+  },
+  {
+    "revision": "6b3e06934cf6b3d7be06",
+    "url": "/kursach/static/js/2.9da09720.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/kursach/static/js/2.9da09720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f08c38fbc1e14ab905",
+    "url": "/kursach/static/js/main.2e2a1923.chunk.js"
+  },
+  {
+    "revision": "2d8830cfe24440a11c41",
+    "url": "/kursach/static/js/runtime-main.fa8ff11b.js"
+  }
+]);
